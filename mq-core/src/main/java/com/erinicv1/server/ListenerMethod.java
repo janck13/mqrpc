c64a0461497094f4b21b732f9db8fd1a2b88f6dd
joey_liuy <993327830@qq.com>
2017-04-26 16:28:27 +0800

    添加 hessian

---
+++ mq-core/src/main/java/com/erinicv1/server/ListenerMethod.java
@@ -0,0 +1,58 @@
+package com.erinicv1.server;
+
+import com.erinicv1.util.HessianSerializerUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
+
+/**
+ * Created by Administrator on 2017/4/26 0026.
+ */
+public class ListenerMethod {
+
+    private Logger logger = LoggerFactory.getLogger(ListenerMethod.class);
+
+    private final static String SPRING_CORRELATION_ID = "spring_reply_correlation";
+
+    private Class serviceApi;
+
+    private Object serviceImpl;
+
+    private ListenerMethod(Class serviceApi,Object serviceImpl){
+        this.serviceApi = serviceApi;
+        this.serviceImpl = serviceImpl;
+    }
+
+    public static ListenerMethod getNewInstance(Class serviceApi,Object serviceImpl){
+        return new ListenerMethod(serviceApi,serviceImpl);
+    }
+
+
+    public Message handleMessage(Message message){
+        logger.info(" " + serviceApi);
+        logger.debug("message receive : " + message);
+
+
+        MessageProperties messageProperties = message.getMessageProperties();
+        boolean compressed = "deflate".equals(messageProperties.getContentEncoding());
+        byte[] response;
+        try{
+            response = HessianSerializerUtil.serverResponseBody(message.getBody(),compressed,serviceImpl,serviceApi);
+
+        }catch (Throwable e){
+            logger.error(" handel message fail : ",e);
+            compressed = false;
+            response = HessianSerializerUtil.serverFautl(message.getBody(),e);
+        }
+
+        MessageProperties properties = new MessageProperties();
+        properties.setContentType("x-application/hessian");
+        if (compressed){
+            properties.setContentEncoding("deflate");
+        }
+        properties.setHeader(SPRING_CORRELATION_ID,messageProperties.getHeaders().get(SPRING_CORRELATION_ID));
+
+        return new Message(response,properties);
+    }
+}
---
+++ mq-core/src/main/java/com/erinicv1/util/HessianSerializerUtil.java
@@ -0,0 +1,135 @@
+package com.erinicv1.util;
+
+import com.caucho.hessian.io.*;
+import com.caucho.hessian.server.HessianSkeleton;
+
+import java.io.*;
+import java.lang.reflect.Method;
+import java.util.zip.Deflater;
+import java.util.zip.DeflaterOutputStream;
+import java.util.zip.Inflater;
+import java.util.zip.InflaterInputStream;
+
+/**
+ * Created by Administrator on 2017/4/26 0026.
+ */
+public class HessianSerializerUtil {
+
+    private static final SerializerFactory serializerFactory = new SerializerFactory();
+
+    public static SerializerFactory getSerializerFactory(){
+        return serializerFactory;
+    }
+
+    public static AbstractHessianInput getHessianInput(InputStream inputStream){
+        AbstractHessianInput input = new Hessian2Input(inputStream);
+        input.setSerializerFactory(serializerFactory);
+        return input;
+    }
+
+    public static AbstractHessianOutput getHessianOutput(OutputStream outputStream){
+        AbstractHessianOutput output = new Hessian2Output(outputStream);
+        output.setSerializerFactory(serializerFactory);
+        return output;
+    }
+
+    public static AbstractHessianOutput getHessianOutput(HessianInputFactory.HeaderType headerType,OutputStream outputStream){
+        AbstractHessianOutput output;
+        HessianFactory factory = new HessianFactory();
+        switch (headerType){
+            case CALL_1_REPLY_1:
+                output = factory.createHessianOutput(outputStream);
+                break;
+            case CALL_1_REPLY_2:
+            case HESSIAN_2:
+                output = factory.createHessian2Output(outputStream);
+                break;
+            default:
+                throw new IllegalStateException(headerType + " is unknown hessian call");
+        }
+
+        return output;
+    }
+
+    public static Object clienResponseBody(byte[] response, Method method,boolean isCompress)throws Throwable{
+        AbstractHessianInput input;
+        InputStream is = new ByteArrayInputStream(response);
+        if (isCompress){
+            is = new InflaterInputStream(is,new Inflater(true));
+        }
+        int code = is.read();
+        if (code == 'H'){
+            is.read();
+            is.read();
+            input = getHessianInput(is);
+            return input.readReply(method.getReturnType());
+        }else if (code == 'r'){
+            is.read();
+            is.read();
+            input = getHessianInput(is);
+            input.startReplyBody();
+            Object object = input.readReply(method.getReturnType());
+            input.completeReply();
+            return object;
+        }else {
+            throw new HessianProtocolException((char)code + " is unknown code ");
+        }
+    }
+
+    public static byte[] clientRequestBody(Object[] requests, Method method, boolean isCompress)throws IOException{
+        OutputStream os;
+        ByteArrayOutputStream payload = new ByteArrayOutputStream(256);
+        String methodName = method.getName();
+
+        if (isCompress){
+            Deflater deflater = new Deflater(Deflater.DEFAULT_COMPRESSION,true);
+            os = new DeflaterOutputStream(payload,deflater);
+        }else {
+            os = payload;
+        }
+        AbstractHessianOutput out = getHessianOutput(os);
+        out.call(methodName,requests);
+        if (os instanceof DeflaterOutputStream){
+            ((DeflaterOutputStream) os).finish();
+        }
+
+        out.flush();
+        return payload.toByteArray();
+    }
+
+    public static byte[] serverResponseBody(byte[] requests, boolean isCompress,Object serviceImpl,Class serviceApi)throws Exception{
+        InputStream inputStream = new ByteArrayInputStream(requests);
+        if (isCompress){
+            inputStream = new InflaterInputStream(inputStream, new Inflater(true));
+        }
+        ByteArrayOutputStream bout = new ByteArrayOutputStream();
+        OutputStream out;
+        if (isCompress){
+            Deflater deflater = new Deflater(Deflater.DEFAULT_COMPRESSION,true);
+            out = new DeflaterOutputStream(bout,deflater);
+        }else {
+            out = bout;
+        }
+
+        HessianSkeleton skeleton = new HessianSkeleton(serviceImpl,serviceApi);
+        skeleton.invoke(inputStream,out,serializerFactory);
+        if (out instanceof DeflaterOutputStream){
+            ((DeflaterOutputStream) out).finish();
+        }
+        out.close();
+        return bout.toByteArray();
+    }
+
+    public static byte[] serverFautl(byte[] requests, Throwable throwable){
+        try{
+            InputStream is = new ByteArrayInputStream(requests);
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+            AbstractHessianOutput output = getHessianOutput(new HessianInputFactory().readHeader(is),os);
+            output.writeFault(throwable.getClass().getSimpleName(),throwable.getMessage(),throwable);
+            output.close();
+            return os.toByteArray();
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
