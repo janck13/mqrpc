bc70ef60a3197c136183bd8cea85ffe414d19161
EsIstGut <erinic1919@gmail.com>
2018-05-13 19:00:54 +0800

    add new branch

---
+++ client/src/main/java/com/erinicv1/SelectInfo.java
@@ -0,0 +1,9 @@
+package com.erinicv1;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SelectInfo {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SelectInfo.class);
+}
